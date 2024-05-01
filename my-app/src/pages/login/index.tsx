af5c104bc877a28d87c94ba083d9640da700cc8b
broken_Arm <brunotoro1799@gmail.com>
2024-05-01 04:12:32 -0400

    pages

--- my-app/src/pages/login/index.tsx
+++ my-app/src/pages/login/index.tsx
@@ -1,14 +1,28 @@
 import React from "react";
-import { Container, Button } from '@mui/material';
+import { Container, Button, Grid, Paper, Box, Typography, TextField } from '@mui/material';
 
 
 export const LoginPage: React.FC<{}> = () =>{
     return (
-        <Container sx={{mt: 9}} maxWidth="xl">
-            <Button fullWidth variant= "contained">
-                Estamos en Login
-            </Button>
+        <Container maxWidth="xl">
+            <Grid container 
+                direction="row"
+                alignItems="center"
+                justifyContent="center"
+                sx={{minHeight: "100vh"}}
+            >
+                <Grid item>
+                    <Paper sx={{padding:"1.2em", borderRadius:"0.5em"}}>
+                        <Typography variant="h4">Iniciar Sesion</Typography>
+                        <Box component="form">
+                            <TextField/>
+                            <TextField/>
+                            <Button type="submit">Iniciar Sesion</Button>
+                        </Box>
+                    </Paper>
 
+                </Grid>
+            </Grid>
         </Container>
     );
 };
