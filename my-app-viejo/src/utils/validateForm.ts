40ce07a2c5033db9f59788c73d6723efc5c45995
broken_Arm <brunotoro1799@gmail.com>
2024-06-09 16:10:15 -0400

    Kami Modificaciones

--- my-app-viejo/src/utils/validateForm.ts
+++
@@ -1,6 +0,0 @@
-import * as yup from "yup";
-
-export const LoginValidate = yup.object().shape({
-    username: yup.string().trim().required("El correo es requerido"),
-    password: yup.string().trim().required("El password es requerido"),
-});
